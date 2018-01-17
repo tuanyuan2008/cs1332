bd57efcede606388ae849e998a1fd976d9492041
tuanyuan2008 <schen1337@gmail.com>
2018-01-17 11:55:21 -0500

    Organized files

---
+++ hw2/SinglyLinkedList.java
@@ -0,0 +1,80 @@
+/**
+ * Your implementation of a circular singly linked list.
+ *
+ * @author YOUR NAME HERE
+ * @userid YOUR USER ID HERE (e.g. gburdell3)
+ * @GTID YOUR GT ID HERE (e.g. 900000000)
+ * @version 1.0
+ */
+public class SinglyLinkedList<T> implements LinkedListInterface<T> {
+    // Do not add new instance variables or modify existing ones.
+    private LinkedListNode<T> head;
+    private int size;
+
+    @Override
+    public void addAtIndex(T data, int index) {
+
+    }
+
+    @Override
+    public void addToFront(T data) {
+
+    }
+
+    @Override
+    public void addToBack(T data) {
+
+    }
+
+    @Override
+    public T removeAtIndex(int index) {
+
+    }
+
+    @Override
+    public T removeFromFront() {
+
+    }
+
+    @Override
+    public T removeFromBack() {
+
+    }
+
+    @Override
+    public T removeLastOccurrence(T data) {
+
+    }
+
+    @Override
+    public T get(int index) {
+
+    }
+
+    @Override
+    public Object[] toArray() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        // DO NOT MODIFY!
+        return size;
+    }
+
+    @Override
+    public LinkedListNode<T> getHead() {
+        // DO NOT MODIFY!
+        return head;
+    }
+}
